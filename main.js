400b2e3caf7e75ba3b12107fe8cab7ca897820db
Gaurik-Gupta <125227706+Gaurik-Gupta@users.noreply.github.com>
2024-03-17 18:59:07 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
-function preLoad(){
+noseX = 0;
+noseY = 0;
 
+function preload() {
+    moustache = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
-function setup(){
-    canvas = createCanvas(400, 300);
+
+function setup() {
+    canvas = createCanvas(300, 300);
     canvas.center();
-}
-function draw(){
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
 
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
-function save(){
-    save("picture.png");
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(moustache, noseX - 18.5, noseY - 5, 40, 40);
+}
+
+function take_snapshot() {
+    save('funny_face.png');
 }
